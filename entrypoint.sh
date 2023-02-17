5e8a4ce7faca4ae546239b719a02dec498b6c18c
Ulf Lilleengen <lulf@redhat.com>
2023-02-17 10:19:09 +0100

    output rationale

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-cat $GITHUB_WORKSPACE/$3 | /seedwing-policy-cli -p $1 eval --name $2 >> $GITHUB_OUTPUT
+echo "Policy dir: $1"
+echo "Policy: $2"
+echo "Input file $GITHUB_WORKSPACE/$3"
+
+rationale=$(cat $GITHUB_WORKSPACE/$3 | RUST_LOG=trace /seedwing-policy-cli -p $1 eval --name "$2")
+ret=$?
+
+echo "Rationale: $rationale"
+echo "rationale=$rationale" >> $GITHUB_OUTPUT
+
+exit $ret
+
